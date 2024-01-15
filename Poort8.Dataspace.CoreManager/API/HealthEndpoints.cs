20dea22729064706142df83bf33e52fe15678c60
Amy Nowé <amy@poort8.nl>
2024-01-15 15:33:04 +0100

    Added health endpoint

---
+++ Poort8.Dataspace.CoreManager/API/HealthEndpoints.cs
@@ -0,0 +1,16 @@
+﻿namespace Poort8.Dataspace.CoreManager.API;
+
+public static class HealthEndpoints
+{
+    public static void MapHealthEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/health").WithTags("Health");
+
+        group.MapGet("", () =>
+        {
+            return TypedResults.Ok("Hello from the Dataspace Core Manager");
+        })
+        .WithName("Health")
+        .WithOpenApi();
+    }
+}
--- Poort8.Dataspace.CoreManager/Program.cs
+++ Poort8.Dataspace.CoreManager/Program.cs
@@ -45,5 +45,6 @@ app.MapRazorComponents<App>()
 
 app.MapFeatureEndpoints();
 app.MapAuthorizationEndpoints();
+app.MapHealthEndpoints();
 
 app.Run();
