ed3a1200674fc8ca0ecff850e50c26d094bbfaca
Amy Nowé <amy@poort8.nl>
2024-02-26 14:18:26 +0100

    Added enforce logs to audit logs page

--- Poort8.Dataspace.CoreManager/AuditLogs/Index.razor.cs
+++ Poort8.Dataspace.CoreManager/AuditLogs/Index.razor.cs
@@ -13,10 +13,12 @@ public partial class Index
     public required IAuthorizationRegistry AuthorizationRegistry { get; set; }
 
     public IReadOnlyList<EntityAuditRecord>? EntityAuditRecords;
+    public IReadOnlyList<EnforceAuditRecord>? EnforceAuditRecords;
 
     protected override async Task OnInitializedAsync()
     {
         StateContainer.OnChange += StateHasChanged;
         EntityAuditRecords = (await AuthorizationRegistry!.GetEntityAuditRecords()).OrderByDescending(o => o.Timestamp).Take(25).ToList();
+        EnforceAuditRecords = (await AuthorizationRegistry!.GetEnforceAuditRecords()).OrderByDescending(o => o.Timestamp).Take(25).ToList();
     }
 }
