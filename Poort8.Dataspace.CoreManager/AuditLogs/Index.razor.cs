1ca391f122bfb12e7022dfed3782692991e5385f
Amy Nowé <amy@poort8.nl>
2024-02-26 13:58:25 +0100

    Add audit logs page with entity logs

---
+++ Poort8.Dataspace.CoreManager/AuditLogs/Index.razor.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Components;
+using Poort8.Dataspace.AuthorizationRegistry;
+using Poort8.Dataspace.AuthorizationRegistry.Audit;
+using Poort8.Dataspace.CoreManager.Services;
+
+namespace Poort8.Dataspace.CoreManager.AuditLogs;
+
+public partial class Index
+{
+    [Inject]
+    public required StateContainer StateContainer { get; set; }
+    [Inject]
+    public required IAuthorizationRegistry AuthorizationRegistry { get; set; }
+
+    public IReadOnlyList<EntityAuditRecord>? EntityAuditRecords;
+
+    protected override async Task OnInitializedAsync()
+    {
+        StateContainer.OnChange += StateHasChanged;
+        EntityAuditRecords = (await AuthorizationRegistry!.GetEntityAuditRecords()).OrderByDescending(o => o.Timestamp).Take(25).ToList();
+    }
+}
